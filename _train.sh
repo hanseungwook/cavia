04b0ffea98aa09717f955f7d1a972702bbe4e75c
Dong Ki Kim <dkkim93@mit.edu>
2020-03-17 12:08:46 -0400

    Added train script and requirements.txt to preveent issues caused by incorrect library versions

---
+++ _train.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+function print_header(){
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+    echo $1
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+}
+
+# Directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+
+# Virtualenv
+cd $DIR
+virtualenv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+# # Comment for using GPU
+# export CUDA_VISIBLE_DEVICES=-1
+
+# Begin experiment
+cd $DIR
+for seed in {1..1}
+do
+    python3.6 regression/main.py
+done
