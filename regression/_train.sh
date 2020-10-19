deeb8c443e7c88e3a042f6107c1d96d14dc1fa71
Dong-Ki Kim <dkkim93@mit.edu>
2020-10-19 14:53:42 -0400

    Train script update

--- regression/_train.sh
+++ regression/_train.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
-function print_header(){
-    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-    echo $1
-    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-}
 
-# Directory of this script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
-
-# Virtualenv
-cd $DIR
+# Load python virtualenv
 virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 
-# # Comment for using GPU
-# export CUDA_VISIBLE_DEVICES=-1
+# Comment for using GPU
+export CUDA_VISIBLE_DEVICES=-1
 
 # Append PYTHONPATH that points to gym-minigrid
 export PYTHONPATH=/home/dongki/research/lids/git/gym-minigrid:$PYTHONPATH
 
 # Begin experiment
-cd $DIR
-for seed in {1..1}
-do
-    # python3.6 simple_meta_example.py
-    python3.6 main_huh.py
-done
+ python3.6 main.py
