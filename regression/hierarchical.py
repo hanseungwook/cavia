f25858abfa1103e1380e872b2a6dde8ae0c0f5b1
Seungwook Han <sh3264@coumbia.edu>
2020-12-20 18:14:23 +0900

    Restructured train loop to prevent gradients to accumulating when not supposed to

--- regression/hierarchical.py
+++ regression/hierarchical.py
@@ -216,12 +216,11 @@ def optimize(model, dataloader, level, args_dict, optimizer, reset, status, devi
                 #     inputs, targets = task_batch
                 #     task_batch = [inputs.to(device), targets.to(device)]
 
-                loss = model(task_batch, level, status=status)[0]     # Loss to be optimized
-
                 if cur_iter >= max_iter:    # train/optimize up to max_iter # of batches
                     if level == 1 and param_all[level].numel() > 0:
                         logger.log_ctx(dataloader.task_name, param_all[level].detach().squeeze().cpu().numpy())   
                     return False #loss_all   # Loss-profile
+                loss = model(task_batch, level, status=status)[0]     # Loss to be optimized
 
                 ## loss.backward() & optim.step()
                 if reset:
