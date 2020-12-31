1cb3850900475e2842d8ed479582d9076a517ddd
Dong-Ki Kim <dkkim93@mit.edu>
2020-12-31 13:26:36 -0500

    Argument cleanup

--- regression/arguments.py
+++ regression/arguments.py
@@ -2,46 +2,43 @@ import argparse
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='CAVIA (Regression experiments)')
+    parser = argparse.ArgumentParser(description='Toy example experiment')
 
     # Arguments for algorithm
     parser.add_argument(
-        "--task", type=str, choices=["empty", "unlock", "mixture"],
-        help="Problem to solve")
-    parser.add_argument(
-        "--network_arch", type=int, nargs='+', default=[1, 100, 100, 1],
+        "--network_arch", type=int, nargs='+', default=[-1, 100, 100, -1],
         help="Architecture of neural network")
     parser.add_argument(
-        "--lrs", type=float, nargs='+', default=[0.005, 0.005, 0.005],
+        "--learning-rates", type=float, nargs='+', default=[0.005, 0.005, 0.005],
         help="lr for inner-loop, midloop, outerloop")
     parser.add_argument(
-        "--max-iters", type=int, nargs='+', default=[2, 2, 10000],
+        "--max-iterations", type=int, nargs='+', default=[2, 2, 10000],
         help="optim_iter for inner-loop, midloop, outerloop")
     parser.add_argument(
-        "--batch", type=int, nargs='+', default=[20, 20, 1],
+        "--batch", type=int, nargs='+', default=[20, 20, 2],
         help="number of trajectories, tasks (e.g., goal locations), super-tasks (e.g., empty)")
     parser.add_argument(
-        "--n_contexts", type=int, nargs='+', default=[5, 5],
+        "--n-contexts", type=int, nargs='+', default=[5, 5],
         help="number of context variables: phi0, phi1")
     parser.add_argument(
-        "--is-hierarchical-learning", action="store_true",
+        "--is-hierarchical", action="store_true",
         help="If True, perform hierarchical meta-learning")
-
-    # Arguments for reinforcement learning settings
     parser.add_argument(
         "--discount", type=float, default=0.95,
         help="Discount factor")
     parser.add_argument(
         "--tau", type=float, default=0.95,
         help="Lambda factor in GAE")
+
+    # Arguments for env
+    parser.add_argument(
+        "--task", type=str, choices=["velocity", "accelation", "mixture"],
+        help="Problem to solve")
     parser.add_argument(
         "--ep-max-timestep", type=int, default=100,
         help="Episode horizon")
 
     # Arguments for misc
-    parser.add_argument(
-        "--log-name", type=str, default="",
-        help="Logging name")
     parser.add_argument(
         "--seed", type=int, default=1,
         help="Seed for reproducibility")
@@ -53,9 +50,9 @@ def parse_args():
 
     # Set args log name
     args.log_name = \
-        "task::%s_lrs::%s_max_iters::%s_batch::%s_args.n_contexts::%s_is_hierarchical_learning::%s_prefix::%s" % (
-            args.task, "-".join(str(item) for item in args.lrs), "-".join(str(item) for item in args.max_iters),
-            "-".join(str(item) for item in args.batch), "-".join(str(item) for item in args.n_contexts),
-            args.is_hierarchical_learning, args.prefix)
+        "task::%s_learning_rates::%s_max_iterations::%s_batch::%s_args.n_contexts::%s_is_hierarchical::%s_prefix::%s" % (
+            args.task, "-".join(str(item) for item in args.learning_rates),
+            "-".join(str(item) for item in args.max_iterations), "-".join(str(item) for item in args.batch),
+            "-".join(str(item) for item in args.n_contexts), args.is_hierarchical, args.prefix)
 
     return args
