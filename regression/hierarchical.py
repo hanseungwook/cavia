f80722e0f9211b96c8f5b1a44928a5461229438e
Seungwook Han <sh3264@coumbia.edu>
2021-01-13 21:26:40 +0900

    Fixed dp level max

--- regression/hierarchical.py
+++ regression/hierarchical.py
@@ -93,7 +93,6 @@ class Hierarchical_Model(nn.Module):            # Bottom-up hierarchy
             
         else:
             self.decoder_model = decoder_model
-            self.level_max = len(decoder_model.parameters_all)
             
         self.decoder_model  = nn.DataParallel(decoder_model)
         self.n_contexts = n_contexts
@@ -105,7 +104,7 @@ class Hierarchical_Model(nn.Module):            # Bottom-up hierarchy
                           }
         self.device     = decoder_model.device
 
-        self.level_max = decoder_model.parameters_all
+        self.level_max = len(decoder_model.parameters_all)
         # self.adaptation = optimize if encoder_model is None else encoder_model 
 
     def forward(self, task_batch, level=None, optimizer=SGD, reset=True, return_outputs=False, status='', viz=None):        # def forward(self, task_batch, ctx_high = [], optimizer = manual_optim, outerloop = False, grad_clip = None): 
