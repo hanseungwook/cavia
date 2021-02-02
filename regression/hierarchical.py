1476fdcb2bf080ba472ccb24c5949f2368ed1caa
Seungwook Han <sh3264@coumbia.edu>
2021-02-02 16:18:47 +0900

    Using refactored make_tasks

--- regression/hierarchical.py
+++ regression/hierarchical.py
@@ -42,55 +42,6 @@ print_optimize_level_over = False
 
 ###################################
 
-def make_tasks(task_names, classes):
-    task_func_dict = {}
-    task_func_dict['train'] = []
-    task_func_dict['test'] = []
-    for task in task_names:
-        if task == 'sine':
-            task_func_dict['train'].append(sample_sin_fnc)
-            # task_func_list.append(sample_sin_fnc)
-        elif task == 'linear':
-            task_func_dict['train'].append(sample_linear_fnc)
-            # task_func_list.append(sample_linear_fnc)
-        elif task == 'celeba':
-            task_func_dict['train'].append(sample_celeba_img_fnc)
-            # task_func_list.append(sample_celeba_img_fnc)
-        elif task == 'cifar10':
-            # task_func_dict['train'] = [partial(sample_cifar10_img_fnc, l) for l in range(5)]
-            # task_func_dict['test'] = [partial(sample_cifar10_img_fnc, l) for l in range(5,10)]
-            task_func_dict['train'].extend([partial(sample_cifar10_img_fnc, l) for l in range(7)])
-            task_func_dict['test'].extend([partial(sample_cifar10_img_fnc, l) for l in range(7,10)])
-        elif task == 'celeba_airplane':
-            task_func_dict['train'].extend([partial(sample_cifar10_img_fnc, 0), sample_celeba_img_fnc])
-            task_func_dict['test'].extend([partial(sample_cifar10_img_fnc, 0), sample_celeba_img_fnc])          
-
-            # for l in range(1):
-            #     task_func_list.append(partial(sample_cifar10_img_fnc, l))
-        elif task == 'airplane':
-            task_func_dict['train'].extend([partial(sample_cifar10_img_fnc, 0)])
-        elif task == 'hier-imagenet':
-            task_func_dict['train'] = create_hier_imagenet_supertasks(data_dir='/disk_c/han/data/ImageNet/', info_dir='./imagenet_class_hierarchy/modified', level=4)
-            # task_func_list = create_hier_imagenet_supertasks(data_dir='/disk_c/han/data/ImageNet/', info_dir='./imagenet_class_hierarchy/modified', level=4)
-        elif task == 'mnist':
-            if len(classes) <= 0:
-                classes = list(range(0, 10))
-            task_func_dict['train'].extend([partial(sample_mnist_img_fnc, l) for l in classes])
-        elif task == 'fashion_mnist':
-            if len(classes) <= 0:
-                classes = list(range(0, 10))
-            task_func_dict['train'].extend([partial(sample_fashion_mnist_img_fnc, l) for l in classes])
-        elif task == 'mnist_fmnist':
-            if len(classes) <= 0:
-                classes = list(range(0, 10))
-            task_func_dict['train'].extend([partial(sample_mnist_img_fnc, l) for l in classes])
-            task_func_dict['train'].extend([partial(sample_fashion_mnist_img_fnc, l) for l in classes])
-        else:
-            raise Exception('Task not implemented/undefined')
-
-    print(task_func_dict)
-    return task_func_dict
-
 def get_hierarchical_task(task_list, classes, k_batch_dict, n_batch_dict):
     task_func_list = make_tasks(task_list, classes)
     task = Hierarchical_Task(task_func_list, (k_batch_dict, n_batch_dict))
