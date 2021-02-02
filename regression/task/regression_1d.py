a89efc6700704a1f50c7c7b08bbf95dc62929638
Seungwook Han <sh3264@coumbia.edu>
2021-02-02 15:37:25 +0900

    Implemented all regression fns (cubic & quadratic) & replicating 1-level

--- regression/task/regression_1d.py
+++ regression/task/regression_1d.py
@@ -38,8 +38,11 @@ def sample_sin_fnc(sample_type):
 def sample_linear_fnc(sample_type):
     return regression_input_function, get_linear_function(*get_linear_params())
 
+def sample_quadratic_fnc(sample_type):
+    return regression_input_function, get_quadratic_function(*get_quadratic_params())
 
-
+def sample_cubic_fnc(sample_type):
+    return regression_input_function, get_cubic_function(*get_cubic_params())
 
 
 ########################
@@ -69,6 +72,47 @@ def get_linear_function(slope, bias):
         return slope * x + bias
 
     return linear_function
+
+def get_quadratic_params():
+    slope1 = np.random.uniform(-0.2, 0.2)
+    slope2 = np.random.uniform(-2.0, 2.0)
+    bias = np.random.uniform(-3., 3.)
+
+    return slope1, slope2, bias
+
+def get_quadratic_function(slope1, slope2, bias):
+    def quadratic_function(x):
+        if isinstance(x, torch.Tensor):
+            return slope1 * torch.pow(x, 2) + slope2 * x + bias
+        else:
+            return slope1 * np.squre(x, 2) + slope2 * x + bias
+
+    return quadratic_function
+
+def get_cubic_params():
+    slope1 = np.random.uniform(-0.1, 0.1)
+    slope2 = np.random.uniform(-0.2, 0.2)
+    slope3 = np.random.uniform(-2.0, 2.0)
+    bias = np.random.uniform(-3., 3.)
+
+    return slope1, slope2, slope3, bias
+
+def get_cubic_function(slope1, slope2, slope3, bias):
+    def cubic_function(x):
+        if isinstance(x, torch.Tensor):
+            return \
+                slope1 * torch.pow(x, 3) + \
+                slope2 * torch.pow(x, 2) + \
+                slope3 * x + \
+                bias
+        else:
+            return \
+                slope1 * np.power(x, 3) + \
+                slope2 * np.power(x, 2) + \
+                slope3 * x + \
+                bias
+
+    return cubic_function
 
 
 
