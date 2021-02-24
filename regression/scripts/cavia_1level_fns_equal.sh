c87f654fe53bbd9fedc805270999515d28fb1c16
Seungwook Han <sh3264@coumbia.edu>
2021-02-24 14:26:51 +0900

    Fixed logging params to match int

--- regression/scripts/cavia_1level_fns_equal.sh
+++ regression/scripts/cavia_1level_fns_equal.sh
@@ -11,7 +11,7 @@ python3 main_huh.py \
 --task sine+line_lv2 --n_contexts 4 0 --n_iters 3 0 5000 --for_iters 3 0 1 \
 --architecture 1 40 40 1 --lrs 0.03 0.03 0.001 --test_intervals 1 0 1 \
 --k_batch_train 10 10000 2 --n_batch_train 10 50 2 --k_batch_test 101 10000 2 --n_batch_test 101 1 1 \
---log-name sine+line_lv1 --log_level_loss True True True --log_level_ctx True True False --task_separate_levels True True True \
+--log-name sine+line_lv1 --log_level_loss 0 1 2 --log_level_ctx 0 1 --task_separate_levels 0 1 2 \
 --private 
 
 echo "Run completed at:- "
