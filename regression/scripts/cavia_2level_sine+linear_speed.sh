6c88d03fa61153b5ec421eba567941ce5ef0d73d
Seungwook Han <sh3264@coumbia.edu>
2021-03-02 16:16:22 +0900

    Added speed logging

--- regression/scripts/cavia_2level_sine+linear_speed.sh
+++ regression/scripts/cavia_2level_sine+linear_speed.sh
@@ -9,6 +9,7 @@ conda activate $PYTHON_VIRTUAL_ENVIRONMENT
 
 echo " Run started at:- "
 date
+START_TIME=$(date +%s)
 
 CUDA_VISIBLE_DEVICES="" python3 main_huh.py \
 --task sine+line --n_contexts 2 2 --max_iters 3 3 100 --for_iters 3 3 1 \
@@ -17,5 +18,7 @@ CUDA_VISIBLE_DEVICES="" python3 main_huh.py \
 --log-name sine+line_lv2_cpu_100_10_2 --log_loss_levels 0 1 2 --task_separate_levels 0 1 2 \
 --print_levels 2 --private \
 
+END_TIME=$(date +%s)
+echo "Time: $(($END_TIME - $START_TIME))"
 echo "Run completed at:- "
 date
