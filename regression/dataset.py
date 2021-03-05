2dd97bdd60bf3aec9c9454e6d351a38abd613e09
Ben Huh <huh@ibm.com>
2021-03-05 00:39:11 -0800

    Meta_DataLoader(DataLoader)

--- regression/dataset.py
+++ regression/dataset.py
@@ -1,5 +1,5 @@
 import random
-from torch.utils.data import Dataset #, DataLoader #, Subset
+from torch.utils.data import Dataset, DataLoader #, Subset
 from torch.utils.data.sampler import RandomSampler, BatchSampler
 
 # import IPython
@@ -26,20 +26,19 @@ def get_Dataset(data, target = None):
 
 class Basic_Dataset(Dataset):
     def __init__(self, input, target=None):
+        if target is not None:
+            assert len(input) == len(target)
+
         self.input = input
         self.target = target
         self.named_input = isinstance(self.input[0], (str,int))
-
-        if target is not None:
-            assert len(input) == len(target)
 
     def __len__(self):
         return len(self.input)
     
     def __getitem__(self, idx):
         if self.target is None:
-            pass
-            # return self.input[idx]
+            return self.input[idx]
         else: 
             idx_output = self.input[idx] if self.named_input else idx
             return self.input[idx], self.target[idx], idx_output
@@ -66,27 +65,37 @@ class Basic_Dataset_LQR(Dataset):  # Temporary code. to be properly written
 
 ##################################
 # Meta_DataLoader
-class Meta_DataLoader():
+class Meta_DataLoader(DataLoader):
     def __init__(self, dataset, batch_size):
-        self.dataset = dataset             # pre-sampled list of tasks
-        self.minibatch_size = max(1, min(batch_size, len(dataset)))   #   1<=batch_size<=len(dataset)
+        batch_size  = max(1, min(batch_size, len(dataset)))          # assert  1<=batch_size<=len(dataset)
+        super().__init__(dataset, batch_size)
 
-    def create_mini_batches(self):         # Create indices of batches
-        batch_idx = list(BatchSampler(RandomSampler(self.dataset), self.minibatch_size, drop_last=True))
+    def create_mini_batches(self):                      # Create list of minibatch indices
+        batch_idx = list(BatchSampler(RandomSampler(self.dataset), self.batch_size, drop_last=True))
         
         # since collate does not work for tasks (only works for torch.tensors / arrays ...)
         # Create dataset of minibatches of the form [mini-batch of (hierarchical tasks), ...]
         mini_dataset = []
         for mini_batch_idx in batch_idx:  
-            mini_batch = [self.dataset[idx_] for idx_ in mini_batch_idx]  # always same mini-batch.. Todo: change each time? 
-            mini_dataset.append(mini_batch)
-
+            mini_dataset.append( [self.dataset[idx_] for idx_ in mini_batch_idx] )
         return mini_dataset
 
     def __len__(self):
         return len(self.dataset)
 
     def __iter__(self):
-        mini_dataset = self.create_mini_batches()
+        # print('iter!')
+        mini_dataset = self.create_mini_batches()  # create different set of mini-batch each time __iter__ is called. 
         return iter(mini_dataset)
 
+########################
+# testing code 
+
+def test_dataloader():
+    dataset = Basic_Dataset(list(range(6)), list(range(6)))
+    dataloader = Meta_DataLoader(dataset, batch_size=2)  # dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+
+    for i in range(4):
+        print('new iter!')
+        for data in dataloader:
+            print(data)
